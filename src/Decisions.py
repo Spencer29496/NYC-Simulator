3e2d606147a5665bf26378b6828cac545da2bdd0
Spencer Mines <smines1@binghamton.edu>
2021-12-02 18:02:25 -0500

    h

--- src/Decisions.py
+++ src/Decisions.py
@@ -1,10 +1,31 @@
-<<<<<<< HEAD
-=======
 import pygame
-from src import button
+import time
+from src import controller
+from pygame import mixer
 
 class Decisions:
-	def __init__(self):
-		start_bt = pygame.image.load("assets/startbt.png")
-		self.start_butt = button.Button(458, 540, start_bt, 0.3)
->>>>>>> d01ce363f24846a4ed681fa589e8c893bc98b25e
+
+    def __init__(self):
+        pygame.init()
+        mixer.init
+        self.window_width, self.window_height = pygame.display.get_surface().get_size()
+
+    def startScreen(self,screen, background):
+        background = pygame.image.load("assets/startscreen.jpg")
+        screen.blit(background,(0,0))
+        pygame.display.set_caption('New York City Simulator')
+        fontObj = pygame.font.Font("assets/Fancy.ttf", 50)
+        textSurfaceObj = fontObj.render('New York City Simulator', True, (255,255,255), None)
+        textRectObj = textSurfaceObj.get_rect(center = (450,100))
+        mixer.music.load("assets/Frank_Sinatra.wav")
+        mixer.music.play()
+        screen.blit(textSurfaceObj,textRectObj)
+
+    def bar(self,screen):
+        bar = pygame.image.load("assets/bar.jpg")
+        self.background = pygame.transform.scale(bar, (self.window_width, self.window_height))
+        self.screen.blit(self.background,(0,0))
+        mixer.music.load("assets/bar_music.wav")
+        mixer.music.play()
+        pygame.display.update()
+
